6a73a5eb165028f96d6e3d1211fb46a2dac7a8df
sd44 <sd44sd44@yeah.net>
2025-04-25 18:59:07 +0800

    aa

--- app/routes/dashboard/newVisit.tsx
+++ app/routes/dashboard/newVisit.tsx
@@ -11,7 +11,7 @@ export const Route = createFileRoute('/dashboard/newVisit')({
 function RouteComponent() {
   return (
     <div className="mx-4 flex flex-col gap-2 overflow-auto p-2">
-      <h3>新增企业信息</h3>
+      <h3>新增走访记录</h3>
       <VisitForm comps={Route.useLoaderData().comps} />;
     </div>
   );
